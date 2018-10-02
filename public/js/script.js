c86a8884cbc3c4bc189139074a48d015b3bde3a9
Jesse McFadden <jessemcbrennan@gmail.com>
2018-10-02 15:54:49 -0600

    Add working color randomizer to script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,25 +1,64 @@
+let colorsArray = []
+
+randomColorGenerator = () => {
+  // let colorsArray = []
+  if(colorsArray.length < 5) {
+  for (i = 0; i < 5; i++) {
+    let randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);}).toUpperCase();
+    colorsArray.push(randomColor)
+  }
+}
+  return colorsArray
+}
 
 $(document).ready(() => {
-  fetch('api/v1/colors')
-  .then(response => {
-    return response.json()
-  }).then(colors => {
+  const colors = randomColorGenerator()
   displayColors(colors)
-  })
 })
+ 
+// $(document).ready(() => {
+//   fetch('api/v1/colors')
+//   .then(response => {
+//     return response.json()
+//   }).then(colors => {
+//   displayColors(colors)
+//   })
+// })
 
 displayColors = (colors) => {
+  $('.colors').empty('.card')
   return mappedColors = colors.map(color => {
-    return $('.colors').prepend(`<div class="card" style="background-color:${color.color}">${color.color}</div>`)
+    return $('.colors').prepend(`<div class="card" style="background-color:${color}">${color}</div>`)
   })
 }
 
-$('.user-input').on('keypress', () => {
+
+postColors = () => {
+  const savedArray = colorsArray
+  return fetch('/api/v1/colors', { method: 'POST', headers:  {
+    "Content-Type": "application/json"},
+    body: JSON.stringify(this.savedColors)
+}).then(response => response.json())
+}
+
+$(this).parent('card').on("click", function() {
+  console.log('card click')
+})
+
+$('body').on("keypress",(e) => {
+  if(e.keyCode == 32) {
+    colorsArray = []
+    let colors = randomColorGenerator();
+    displayColors(colors);
+  }
+})
+
+$('.user-input').on("keypress", () => {
   console.log('hello')
 })
 
-$('.user-click').on('click', () =>{
-  console.log('hi')
+$('.user-click').on('click', () => {
+  postColors()
 })
 
 
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 app.set('port', 3000);
+
+app.use(bodyParser.json())
 
 app.use(express.static('public/'));
 app.locals.title ='Palette Picker'
