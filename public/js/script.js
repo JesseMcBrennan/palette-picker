0dd95e11a4f03b7fe1e2a26d379cbca13234fde8
Jesse McFadden <jessemcbrennan@gmail.com>
2018-10-07 15:16:39 -0600

    Refactor color to object

--- public/js/script.js
+++ public/js/script.js
@@ -1,80 +1,100 @@
 
-let colorsArray = []
+let initialArray = []
+let colorsArray = [
+  { colorValue: '#E5E5E5', locked: false },
+  { colorValue: '#2A31D7', locked: false },
+  { colorValue: '#D72AC6', locked: false },
+  { colorValue: '#38D72A', locked: false },
+  { colorValue: '#D7C62A', locked: false }
+]
 
-$(document).ready(() => {
-  const colors = randomColorGenerator()
-  displayColors(colors)
+$('.palette-name').submit(savePalette)
+
+$('.user-click').on('click', () => {
+  postColors()
 })
 
-randomColorGenerator = () => {
-  if(colorsArray.length < 5) {
-  for (i = 0; i < 5; i++) {
-    let randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);}).toUpperCase();
-    colorsArray.push(randomColor)
-  }
-}
-  return colorsArray
+$(document).ready(() => {
+  getProjects();
+  getPalettes();
+  displayColors()
+})
+
+updateColorGenerator = (array) => {
+  colorsArray.forEach(color => {
+  let randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);}).toUpperCase();
+    color.colorValue = randomColor
+  })
+  return 
 }
 
- 
-$(document).ready(() => {
+function saveProjects() {
+  fetch('api/v1/projects', {
+    method: 'POST',
+    head: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response.json())
+  .then(resonse => console.log('Success'))
+}
+
+function getPalettes() {
+  fetch('api/v1/palettes') 
+  .then(response => {
+    return response.json()
+  }).then(palettes => {
+    
+  })
+}
+
+function getProjects() {
   fetch('api/v1/projects')
   .then(response => {
     return response.json()
-  }).then(colors => {
-  displayColors(colors)
+  }).then(projects => {
+    
   })
+}
+
+$('body').on("keypress",(e) => {
+  if(e.keyCode == 32) {
+    updateColorGenerator()
+    displayColors() 
+  }
 })
-
-// function updateColors() {
-//   $('.colors').each(function(i) {
-//     if($(this).attr('src', $(this).hasClass('locked'))) {
-//       return;
-//     } else {
-//       let color;
-//       if(colors.length) {
-//         color = colors[i]
-//       } else {
-//         randomColorGenerator()
-//       }
-//     }
-//   })
-// }
-
-
-// function updateColors(colors = []) {
-//   currentColors = [];
-//   $('.colors').each(function(i) {
-//     if($(this).attr('img', $(this).hasClass('locked'))) {
-//       } else {
-//     let color;
-//     if(colors.lenght) {
-//       color = colors[i]
-//     } else {
-//       color = randomColorGenerator()
-//     }
-//     }
-//   })
-// }
 
 function toggleLock() {
   let locked = '../assets/lock.svg'
   let unlocked = '../assets/unlock.svg'
 
   $(this).toggleClass('locked');
+  $(this).removeClass('unlocked');
   $(this).attr('src', $(this).hasClass('locked') ? locked : unlocked);
+  $(this).attr('src', $(this).hasClass('') ? unlocked : locked);
 }
 
+
+  
 displayColors = (colors) => {
   $('.colors').empty('.card')
-  return mappedColors = colors.map(color => {
-    return $('.colors').prepend(`<div class="card" style="background-color:${color}">${color}<img class="unlocked" src="../assets/unlock.svg"></img></div>`)
+  if($('.colors').hasClass("locked ")) {
+    console.log('this')
+  }
+  return mappedColors = colorsArray.map(color => {
+    return $('.colors').prepend(`<div class="card" style="background-color:${color.colorValue}">${color.colorValue}<img src="../assets/unlock.svg"></img></div>`)  
+    
+  })
+}
+
+updateColors = () => {
+  colorsArray.forEach(color => {
+    console.log(color)
   })
 }
 
 postColors = () => {
   const savedArray = colorsArray
-  return fetch('/api/v1/palettes', { method: 'POST', headers:  {
+  return fetch('/api/v1/projects ', { method: 'POST', headers:  {
     "Content-Type": "application/json"},
     body: JSON.stringify(savedArray)
 }).then(response => response.json())
@@ -82,22 +102,10 @@ postColors = () => {
 
 $('.colors').on("click", ".card img", toggleLock);
 
-$('body').on("keypress",(e) => {
-  if(e.keyCode == 32) {
-    colorsArray = []
-    let colors = randomColorGenerator();
-    // updateColors()
-    displayColors(colors);
-  }
-})
+function savePalette(event) {
+  console.log('hi')
+}
 
-$('.user-input').on("keypress", () => {
-  console.log('hello')
-})
-
-$('.user-click').on('click', () => {
-  postColors()
-})
 
 
 
--- server.js
+++ server.js
@@ -12,13 +12,6 @@ app.use(bodyParser.json())
 
 app.use(express.static('public/'));
 app.locals.title ='Palette Picker'
-// app.locals.colors = [
-//   {id: 1, color: '#E5E5E5'},
-//   {id: 2, color: '#2A31D7'},
-//   {id: 3, color: '#D72AC6'},
-//   {id: 4, color: '#38D72A'},
-//   {id: 5, color: '#D7C62A'}
-// ]
 
 app.get('/api/v1/projects', (request, response) => {
   database('projects').select()
